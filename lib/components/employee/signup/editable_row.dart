78a474bd8c592b6e874522fd9f52bbd452d1aefc
Bitencure <matheusbgome@gmail.com>
2021-09-02 23:48:53 -0300

    editable rows are now editable

--- lib/components/employee/signup/editable_row.dart
+++ lib/components/employee/signup/editable_row.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class EditableRow extends StatelessWidget {
+class EditableRow extends StatefulWidget {
   const EditableRow(
     this.text, {
     Key key,
@@ -9,26 +9,58 @@ class EditableRow extends StatelessWidget {
   final String text;
 
   @override
+  _EditableRowState createState() => _EditableRowState();
+}
+
+class _EditableRowState extends State<EditableRow> {
+  bool _isEditing = false;
+
+  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 10.0),
       child: Row(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Text(text,
-              textAlign: TextAlign.end,
-              style:
-                  Theme.of(context).textTheme.headline1.copyWith(fontSize: 18)),
+          _isEditing
+              ? IntrinsicWidth(
+                  child: TextFormField(
+                    autofocus: true,
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline1
+                        .copyWith(fontSize: 18),
+                    initialValue: widget.text,
+                    textCapitalization: TextCapitalization.words,
+                    autocorrect: false,
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.zero,
+                      isDense: true,
+                    ),
+                  ),
+                )
+              : Text(
+                  widget.text,
+                  textAlign: TextAlign.end,
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline1
+                      .copyWith(fontSize: 18),
+                ),
           SizedBox(
             width: 10,
           ),
           InkWell(
             child: Icon(
-              Icons.edit,
+              _isEditing ? Icons.check : Icons.edit,
               size: 24,
               color: Theme.of(context).primaryColor,
             ),
-            onTap: () {},
+            onTap: () {
+              setState(() {
+                _isEditing = !_isEditing;
+              });
+            },
             borderRadius: BorderRadius.circular(100),
           ),
         ],
